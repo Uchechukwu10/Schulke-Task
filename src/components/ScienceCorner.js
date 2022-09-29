5772ea113532d2291244378a116e69f06bbb1b7d
Uche <uchaynwankwo@gmail.com>
2022-09-29 17:39:29 +0100

    attempted carousel

--- src/components/ScienceCorner.js
+++ src/components/ScienceCorner.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { React, useEffect, useState } from 'react';
 import ScienceCard from './ScienceCard';
 import covid1 from '../images/covid-1.png';
 import covid2 from '../images/covid-2.png';
@@ -26,12 +26,13 @@ const ScienceCorner = () => {
     description: 'Just as your genes define your outer appearance, they also influence your heart an...',
     postedTime: 20
   }
+
   return (
-    <div className='flex flex-row gap-3 justify-center items-center'>
-      <ScienceCard tag={card1.tag} title={card1.title} desc={card1.description} image={card1.image} time={card1.postedTime}/>
-      <ScienceCard tag={card2.tag} title={card2.title} desc={card2.description} image={card2.image} time={card2.postedTime}/>
-      <ScienceCard tag={card3.tag} title={card3.title} desc={card3.description} image={card3.image} time={card3.postedTime}/>
-    </div>
+    <section className='flex flex-row gap-1 justify-center items-center relative'>
+            <ScienceCard tag={card1.tag} title={card1.title} desc={card1.description} image={card1.image} time={card1.postedTime}/>
+            <ScienceCard tag={card2.tag} title={card2.title} desc={card2.description} image={card2.image} time={card2.postedTime}/>
+            <ScienceCard tag={card3.tag} title={card3.title} desc={card3.description} image={card3.image} time={card3.postedTime}/>
+    </section>
   )
 }
 
