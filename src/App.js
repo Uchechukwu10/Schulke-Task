b3b5ec1afd096d826325c8ca75f893ed8fd4efc8
Uche <uchaynwankwo@gmail.com>
2022-09-29 14:43:51 +0100

    add footer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import './App.css';
+import { useRef, useEffect, useState } from 'react';
 import NavBar from './components/NavBar';
 import Banner from './components/Banner';
 import ScienceCorner from './components/ScienceCorner';
-import ScienceCard from './components/ScienceCard';
 import Footer from './components/Footer';
+import {IoIosArrowUp} from 'react-icons/io';
 
 function App() {
+  const banner = useRef();
+  const [scrolled, setScrolled] = useState(123)
+  const scrollUp = () => {
+    window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+  }
+  const handleScroll = (event) => {
+    // console.log('scrollTop: ', event.currentTarget.scrollTop);
+    // console.log('offsetHeight: ', event.currentTarget.offsetHeight);
+    const scroll = banner.current.getBoundingClientRect();
+    setScrolled(scroll.top);
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [])
   return (
-    <div>
+    <div onScroll={handleScroll} className='final'>
         <NavBar />
-        <Banner />
+        <div ref={banner} onClick={handleScroll}><Banner /></div>
         <ScienceCorner />
         <Footer />
+        <button className={scrolled < -240 ? 'scroll-up p-2' : 'invisible'} onClick={scrollUp}><IoIosArrowUp fontSize='1.3rem' color='white'/></button>
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,51 @@
-import React from 'react'
+import React from 'react';
+import logo from '../images/Schulke-text.png';
+import { FaFacebookF, FaTwitter, FaLinkedinIn } from 'react-icons/fa';
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <section className='footer'>
+      <div className='details flex flex-row pl-20 pr-12 py-12'>
+        <div className='logo flex flex-col w-4/12 px-10 mx-5'>
+           <button href="#" className="flex flex-col py-4 pr-2">
+                <img src={logo} alt="Logo" className="h-7 w-40 mr-2" />
+                <span className='text-white text-xs font-semibold'>Knowledge Transfer</span>
+            </button>
+            <p className='text-white text-base pr-4 text-justify py-4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod Lorem ipsum do
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod Lorem ipsum do
+            </p>
+            <div className='flex py-5 gap-4'>
+              <FaFacebookF fontSize='1.2rem'/>
+              <FaTwitter fontSize='1.2rem'/>
+              <FaLinkedinIn fontSize='1.2rem'/>
+            </div>
+        </div>
+        <div className='quick-links flex flex-col px-16 mx-5'>
+          <h1 className='text-xl font-bold py-5'>Quick Links</h1>
+          <p className='text-sm py-2'>Home</p>
+          <p className='text-sm py-2'>About</p>
+          <p className='text-sm py-2'>Trainings</p>
+          <p className='text-sm py-2'>Contact</p>
+        </div>
+        <div className='topics flex flex-col px-16 mx-5'>
+          <h1 className='text-xl font-bold py-5'>Topics</h1>
+          <p className='text-sm py-2'>Blanket Topics</p>
+          <p className='text-sm py-2'>Country Topics</p>
+          <p className='text-sm py-2'>Product Topics</p>
+        </div>
+        <div className='important-links flex flex-col pl-16 pr-6 mx-5'>
+          <h1 className='text-xl font-bold py-5'>Important Links</h1>
+          <p className='text-sm py-2'>Privacy Policy</p>
+          <p className='text-sm py-2'>Terms and Conditions</p>
+          <p className='text-sm py-2'>Disclaimer</p>
+          <p className='text-sm py-2'>Sitemap</p>
+        </div>
+      </div>
+      <div className='copyright flex items-center justify-center py-2'>
+        <span className='text-sm py-2 text-white'>&copy; Copyright 2022 Schulke</span>
+      </div>
+    </section>
   )
 }
 
